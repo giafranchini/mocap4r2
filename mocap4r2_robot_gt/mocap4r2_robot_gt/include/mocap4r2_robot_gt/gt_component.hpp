2cec03009a96272e5c152ae16808831e53b7b6db
giacomo <giacomo.franchini@polito.it>
2024-02-07 17:53:59 +0100

    feature: rigid body odometry from vicon data

--- mocap4r2_robot_gt/mocap4r2_robot_gt/include/mocap4r2_robot_gt/gt_component.hpp
+++ mocap4r2_robot_gt/mocap4r2_robot_gt/include/mocap4r2_robot_gt/gt_component.hpp
@@ -21,6 +21,8 @@
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 
 #include <vector>
 
@@ -45,23 +47,35 @@ protected:
 
   geometry_msgs::msg::Pose get_pose_from_vector(const std::vector<double> & init_pos);
 
+  void compute_odometry(
+    const tf2::Transform & root2robot_tf,
+    nav_msgs::msg::Odometry::UniquePtr & odom_msg);
+
   tf2::BufferCore tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
   std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
   rclcpp::Subscription<mocap4r2_msgs::msg::RigidBodies>::SharedPtr rigid_body_sub_;
   rclcpp::Service<mocap4r2_robot_gt_msgs::srv::SetGTOrigin>::SharedPtr set_gt_origin_srv_;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odometry_pub_;
 
   std::string root_frame_;
   std::string robot_frame_;
   std::string mocap_frame_;
 
+  std::string rigid_body_topic_;
+  std::string odometry_topic_;
+
   tf2::Transform offset_;
   tf2::Transform gtbody2robot_;
   tf2::Transform mocap2gtbody_;
+  
+  geometry_msgs::msg::PoseStamped prev_pose_;
 
   bool valid_gtbody2robot_{false};
 
+  std::vector<double> pose_covariance_;
+  std::vector<double> twist_covariance_;
 };
 
 }  // namespace mocap4r2_robot_gt
